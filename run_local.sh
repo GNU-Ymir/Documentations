5781c0f4be4fe42c46151dbb140f90ed20e82150
CADOREL Emile <ecadorel@gmail.com>
2021-04-20 18:50:24 +0200

    Update docs

--- run_local.sh
+++ run_local.sh
@@ -1,6 +1,8 @@
-gitbook install .
+#rm -rf _book
+#gitbook install .
 cp -r themes node_modules/
 cp -r gitbook-plugin-spoiler node_modules/
+cp -r gitbook-plugin-code node_modules/
 cp -r prismjs node_modules/
 gitbook serve .
 
